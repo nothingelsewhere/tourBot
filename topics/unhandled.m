a60fa139743176aebb7f1a7f17ba14d01ef7769e
Yeo Kai Yu <yeokaiyu@gmail.com>
2021-11-27 13:09:14 +0800

    Create unhandled.m

---
+++ topics/unhandled.m
@@ -0,0 +1,11 @@
+\ Catches unhandled questions and statements. 
+\ Should be the very last include. 
+\
+
+\ better than an IDK response
+\ Unhandled statements
+Q: $_
+MM Sorry, I don't understand your question. Here are some options:
+M. ${ main-menu }
+--
+
