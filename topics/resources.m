7c9f216b931b604e8a50b27b3f94223de3a1d403
Yeo Kai Yu <yeokaiyu@gmail.com>
2021-11-27 13:10:01 +0800

    Create resources.m

---
+++ topics/resources.m
@@ -0,0 +1,9 @@
+\ this file creates references to IMAGE resources
+
+\ used in start.m
+: img-lantern "https://cdn.pixabay.com/photo/2018/05/27/23/16/ramadan-3434919_1280.jpg" ; 
+: img-jumping "https://cdn.pixabay.com/photo/2020/01/11/13/22/woman-4757533_1280.jpg" ; 
+: img-walking "https://cdn.pixabay.com/photo/2020/03/06/05/38/woman-4906077_1280.jpg" ; 
+: img-thebes "https://cdn.pixabay.com/photo/2016/02/27/00/13/ramses-2-1224938_1280.jpg" ;
+
+
