c9e4162d876baaee9a883cf0262c4bd35d46394c
Yeo Kai Yu <yeokaiyu@gmail.com>
2021-11-27 13:10:50 +0800

    Create questions.m

---
+++ topics/questions.m
@@ -0,0 +1,23 @@
+\ NLB - AI4IMPACT Smart City Chatbot Challenge
+\ 35m 47s
+\ https://youtu.be/h_dbTS_gd9s?t=2150
+
+\ show an image
+IMG: https://cdn.pixabay.com/photo/2015/10/23/12/25/egypt-1002916_1280.jpg
+\ pose the question
+QQ Who built this pyramid?
+\ show possible responses
+\ correct answer is marked with a closing parentheses )
+AA) Khufu
+AA Ramses
+AA Sneferu
+***
+
+IMG: https://cdn.pixabay.com/photo/2014/10/20/10/23/nile-495098_1280.jpg
+QQ How long is the Nile?
+AA 1,200 km 
+AA 5,160 km
+AA) 6,650 km
+AA 7800 km
+***
+
